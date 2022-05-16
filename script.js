58e0f06c960dcdda57b8846c5be5e0362d27a298
Munevera Plojovic <93732677+muneveraplojovic@users.noreply.github.com>
2022-05-16 22:25:33 +0200

    first commit

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function pokreniMe(dugme) {
     message.innerText="HEEEEYYY "
     console.log(dugme);
 }
+function pokreniMe(dugme) {
+    let nazivDugmeta=dugme.innerText
+    let message= document.querySelector("#kliknutoDugme")
+    message.innerText=nazivDugmeta
+}
